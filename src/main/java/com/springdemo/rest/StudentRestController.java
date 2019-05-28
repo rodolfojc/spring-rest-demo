5f0afe35e415a9c1d280ed9612c1ab0ffb808dc0
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-05-28 22:13:15 +0100

    Refactoring Code
    Demo - Better perform

--- src/main/java/com/springdemo/rest/StudentRestController.java
+++ src/main/java/com/springdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +15,24 @@ import com.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	//DEFINE ENDPOINT FOR /STUDENTS
-	@GetMapping("/students")
-	public List<Student> getStudents(){
+	private List<Student> theStudents;
+	
+	//DEFINE @POSTCONTRUCT TO LOAD THE STUDENT DATA ONLY ONCE
+	@PostConstruct
+	public void loadData() {
 		
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Rodolfo", "Carvajal"));
 		theStudents.add(new Student("Lucy", "Lagunas"));
 		theStudents.add(new Student("Juan", "Velasquez"));
 		
+	}
+	
+	
+	//DEFINE ENDPOINT FOR /STUDENTS
+	@GetMapping("/students")
+	public List<Student> getStudents(){
 		return theStudents;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,6 +5,8 @@
 	<P> Welcome Spring REST Demo Web Service </P>
 	<hr>
 	<a href="${pageContext.request.contextPath}/test/hello">Hello!!</a>
+	<hr>
+	<a href="${pageContext.request.contextPath}/api/students">Get Students</a>
 
 </body>
 
